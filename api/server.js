d1e90aad851817b101fca38d5f6e37e4abda0229
Ediccson1 <165943341+Ediccson1@users.noreply.github.com>
2025-05-17 23:07:50 -0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,10 @@
+// api/server.js
+const app = require("../index");
+
+// Verificar que Vercel usa Serverless
+if (process.env.NODE_ENV === "production") {
+  console.log("🚀 API ejecutándose en Vercel (Serverless)");
+}
+
+module.exports = app;
 
