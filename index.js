28db627b7dbd83620a313a63c0565baafc29e0a7
Ediccson1 <165943341+Ediccson1@users.noreply.github.com>
2025-05-20 16:01:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const sequelize = require("./db");
 const productoRoutes = require("./routes/productoRoutes");
 const Producto = require("./models/Producto");
 
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
 // Configuraciones
 app.use(cors());
 app.use(express.json());
